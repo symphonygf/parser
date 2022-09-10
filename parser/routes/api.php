97585de72dd837538ac080409201a44c2efacf4c
Георгий <root@gosha.ru>
2022-09-10 14:53:21 +0300

    Task-2 add parser.

--- parser/routes/api.php
+++ parser/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ImportCarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::namespace('api')->prefix("/v1")->group(
-    function () {
-        Route::get('import', [ImportCarController::class, 'index'])->name('import.car.index');
-    }
-);
